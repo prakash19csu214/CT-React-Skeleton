03b42cccaa2c86671d96b40b3d0679954f564991
prakash19csu214 <prakashsingh112000@gmail.com>
2023-01-17 17:03:19 +0530

    kc

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,47 +1,51 @@
-import React from "react";
-import { useContext } from "react";
-import { MovieContext } from "../../Layouts/Main";
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+    import React from "react";
+    import { useContext } from "react";
+    import { MovieContext } from "../../Layouts/Main";
+    import './index.css'
+    import Skeleton from "react-loading-skeleton";
+    import "react-loading-skeleton/dist/skeleton.css";
 
-function Card() {
-    
+    function Card() {
     const movies = useContext(MovieContext);
-  
-  return (
-    <div className="container">
-    <div className="my-5">
-      {/* <h2 style={{'color': 'white'}}> {movie.Title || <Skeleton />} </h2> */}
-      
 
-      <div className="row_posters">
-        {/* poster */}
+    return (
+        <div className="container">
+          <div className="my-5">
+              {movies ?
+                movies.map((movie, index) => (
+                  <>
+                  <div className="container">
+                    <div className="row mt-3">
+                      <div className="col-12 col-md-4 d-flex justify-content-center">
+                        <img
+                          key={index}
+                          className={"card_poster"}
+                          src={movie.Poster}
+                          alt={movie.Title}
+                        />
+                      </div>
+                      <div className="col-12 col-md-6 offset-md-2">
+                        <div className="row d-flex justify-content-center text-center">
+                        <h2 style={{ color: "white" }}>{movie.Title}</h2>
+                        </div>
+                        <div className="row d-flex justify-content-center">
+                        <h4 style={{ color: "white" }}>{movie.Year}</h4>
+                        </div>
+                        <div className="row d-flex justify-content-center">
+                        <h4 style={{ color: "white" }}>{movie.Runtime}</h4>
+                        </div>
+                        
+                      </div>
+                    </div>
+                    </div>
+                  </>
+                ))
+                : <Skeleton count={5} height={200}/>
+              }
+            </div>
+          </div>
+      );
+    
+    }
 
-        {movies ? (
-          movies.map((movie, index) => (
-            <img 
-              key={index}
-              className={"row_poster"}
-              src={movie.Poster}
-              alt={movie.Title}
-            />
-          ))
-        ) : (
-          <>
-           
-            <Skeleton className="skel" />
-           
-            <Skeleton className="skel" />
-           
-            <Skeleton className="skel" />
-           
-            <Skeleton className="skel" />
-          </>
-        )}
-      </div>
-      </div>
-      </div>
-  );
-}
-
-export default Card;
+    export default Card;
--- src/Pages/Trending/index.js
+++ src/Pages/Trending/index.js
@@ -1,10 +1,8 @@
 import React, { useContext } from "react";
 import Card from "../../Components/Card";
-import { MovieContext } from "../../Layouts/Main";
 import "./index.css";
 
 export default function Trending() {
-  const movies = useContext(MovieContext);
   return (
     <>
       <div className="container">
